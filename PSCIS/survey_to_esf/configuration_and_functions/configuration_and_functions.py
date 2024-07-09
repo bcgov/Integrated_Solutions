267ec96d9711f59870945b7d0d652c2a25dc7d25
Haynes <Tara.Haynes@gov.bc.ca>
2024-07-09 13:29:43 -0700

    Create configuration_and_functions.py

---
+++ PSCIS/survey_to_esf/configuration_and_functions/configuration_and_functions.py
@@ -0,0 +1,10 @@
+# Import Modules
+
+import sys,os
+import os, json, datetime
+from arcgis.gis import GIS
+import xml.etree.ElementTree as ET
+from pathlib import Path
+from zipfile import ZipFile
+from datetime import datetime
+import time
