e592866f810d7d657397bc69572ff896d6139e3f
Haynes <Tara.Haynes@gov.bc.ca>
2024-07-10 14:06:32 -0700

    Update configuration_and_functions.py

--- PSCIS/survey_to_esf/configuration_and_functions/configuration_and_functions/configuration_and_functions.py
+++ PSCIS/survey_to_esf/configuration_and_functions/configuration_and_functions/configuration_and_functions.py
@@ -9,5 +9,27 @@ from zipfile import ZipFile
 from datetime import datetime
 import time
 
-def test_function():
-  print("Wassup?")
+agol_username = os.environ["AGO_USER"]
+agol_password = os.environ["AGO_PASS"]
+agol_url = os.environ["AGO_PORTAL_URL"]
+
+
+def connect_to_agol():
+    """
+    Returns GIS Object. First step for working with ArcGIS API for Python.
+    """
+    start_time = time.time()
+    print(agol_url)
+    try:
+        gis = GIS(agol_url, agol_username, agol_password, verify_cert=False)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Successfully Connected, Elapsed Time:")
+        print(elapsed_time)
+        return gis
+    except:
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Elapsed Time:")
+        print(elapsed_time)
+        print("Could not Make a Connection to ArcGIS Online")
