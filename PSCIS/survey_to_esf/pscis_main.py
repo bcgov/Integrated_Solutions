0a3aeaa066176a8754068967ee798741880406e9
Haynes <Tara.Haynes@gov.bc.ca>
2024-07-10 11:45:46 -0700

    Update pscis_main.py

--- PSCIS/survey_to_esf/pscis_main.py
+++ PSCIS/survey_to_esf/pscis_main.py
@@ -2,3 +2,9 @@ import os, logging
 
 #Set logging level (NOTSET,DEBUG,INFO,WARNING,ERROR,CRITICAL)
 logging_level = logging.INFO
+
+print("Hello World")
+
+import configuration_and_functions as conf
+
+conf.test_function()
