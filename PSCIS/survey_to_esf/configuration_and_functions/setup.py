579c3aa02cbeaed762de8218c54f625bdef4fc71
Haynes <Tara.Haynes@gov.bc.ca>
2024-07-10 11:54:19 -0700

    Create setup.py

---
+++ PSCIS/survey_to_esf/configuration_and_functions/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='configuration_and_functions',
+        version='1.0',
+        description='Custom python functions for GeoBC Business Innovation and Emergency Response',
+        url='https://github.com/bcgov/integration-solutions/tree/main/PSCIS/survey_to_esf/configuration_and_functions',
+        author='Tara Haynes',
+        author_email='Tara.Haynes@gov.bc.ca',
+        license='Aparche',
+        packages=['configuration_and_functions'])
