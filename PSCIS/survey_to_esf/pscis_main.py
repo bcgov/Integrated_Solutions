04810be4c102f009327ea5059ec833cf4a8939e6
Haynes <Tara.Haynes@gov.bc.ca>
2024-07-11 15:14:40 -0700

    Update pscis_main.py

--- PSCIS/survey_to_esf/pscis_main.py
+++ PSCIS/survey_to_esf/pscis_main.py
@@ -3,10 +3,9 @@ import os, logging
 #Set logging level (NOTSET,DEBUG,INFO,WARNING,ERROR,CRITICAL)
 logging_level = logging.INFO
 
-print("Hello World")
-
 import configuration_and_functions as conf
 
 agol_connection = conf.connect_to_agol()
+s3_connection = conf.connect_to_s3()
 
 print(agol_connection)
